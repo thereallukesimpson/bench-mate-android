cfca35231bd945fde1e3433d1245d25462d2a697
Luke Simpson <thereallukesimpson@gmail.com>
2023-06-12 14:25:43 +1000

    Target SDK 34 and latest Gradle/AGP so we can hopefully access setUpEdgeToEdge.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "app.benchmate"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "app.benchmate"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 

0b84625db679c6f22905635008446d7f58dc6508
Luke Simpson <thereallukesimpson@gmail.com>
2023-06-13 15:37:37 +1000

    Add edge-to-edge

--- app/src/main/java/app/benchmate/MainActivity.kt
+++ app/src/main/java/app/benchmate/MainActivity.kt
@@ -3,27 +3,23 @@ package app.benchmate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import app.benchmate.ui.components.BenchMateScaffold
 import app.benchmate.ui.theme.BenchMateTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            BenchMateTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            BenchMateScaffold {
+
             }
         }
     }

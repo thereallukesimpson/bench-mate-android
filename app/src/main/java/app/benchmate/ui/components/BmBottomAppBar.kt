553edc17f7a5548b5dbe3e226cfcac9f11b0775c
Luke Simpson <lukesimpson@thegoodguys.com.au>
2024-02-02 09:47:31 +1100

    Add app name to bottom bar for now

--- app/src/main/java/app/benchmate/ui/components/BmBottomAppBar.kt
+++ app/src/main/java/app/benchmate/ui/components/BmBottomAppBar.kt
@@ -1,10 +1,12 @@
 package app.benchmate.ui.components
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
@@ -18,6 +20,7 @@ fun BenchMateBottomAppBar(
             ) {
                 Icon(Icons.Filled.Home, contentDescription = "Bench")
             }
+            Text(text = "BenchMate")
 //            IconButton(onClick = { /* doSomething() */ }) {
 //                Icon(Icons.Filled.Person, contentDescription = "Team")
 //            }

b221b28b37a5900cb05721f434a46d04891ab217
Luke Simpson <lukesimpson@thegoodguys.com.au>
2024-02-02 07:53:38 +1100

    Add basic alert dialog and show to test

--- app/src/main/java/app/benchmate/ui/features/bench/BenchScreen.kt
+++ app/src/main/java/app/benchmate/ui/features/bench/BenchScreen.kt
@@ -1,23 +1,39 @@
 package app.benchmate.ui.features.bench
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import app.benchmate.ui.components.BenchMateBottomAppBar
 import app.benchmate.ui.components.BenchMateFab
 import app.benchmate.ui.components.BenchMateScaffold
+import app.benchmate.ui.components.BmAlertDialog
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BenchScreen() {
+
+    var openAlertDialog = remember { mutableStateOf(true) }
+
     BenchMateScaffold(
-        bottomBar = { BenchMateBottomAppBar(
-            floatingActionButton = {
+        bottomBar = {
+            BenchMateBottomAppBar {
                 BenchMateFab(
-                    onClick = {}
+                    onClick = { openAlertDialog } // Not working
                 )
             }
-        ) }
+        }
     ) {
-
+        if (openAlertDialog.value) {
+            BmAlertDialog(
+                onDismissRequest = {},
+                onConfirmation = {},
+                dialogTitle = "Add Player",
+                dialogText = "Is this text needed?",
+                icon = Icons.Filled.Person
+            )
+        }
     }
 }

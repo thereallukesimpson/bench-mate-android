10bcadfbd6ede47768a419d09e69612ddb82e76f
Luke Simpson <lukesimpson@thegoodguys.com.au>
2024-02-03 21:41:30 +1100

    Apply capitalisation

--- app/src/main/java/app/benchmate/ui/components/BmInputDialog.kt
+++ app/src/main/java/app/benchmate/ui/components/BmInputDialog.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
@@ -24,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -72,7 +74,9 @@ fun BmInputDialog(
                     value = nameEntered,
                     onValueChange = { nameEntered = it },
                     label = { Text(text = inputLabel) },
-                    singleLine = true
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        capitalization = KeyboardCapitalization.Sentences)
                 )
 
                 Row(

19eb61c6a41a702ce18628605189736c85a658a3
Luke Simpson <lukesimpson@thegoodguys.com.au>
2024-02-03 16:54:11 +1100

    Rename app to container

--- app/src/main/java/app/benchmate/ui/BmContainer.kt
+++ app/src/main/java/app/benchmate/ui/BmContainer.kt
@@ -5,7 +5,7 @@ import app.benchmate.ui.features.bench.BenchScreen
 import app.benchmate.ui.theme.BenchMateTheme
 
 @Composable
-fun BenchMateApp() {
+fun BmContainer() {
     BenchMateTheme {
         // TODO Add navigation but currently just one screen
         BenchScreen()
--- app/src/main/java/app/benchmate/ui/MainActivity.kt
+++ app/src/main/java/app/benchmate/ui/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        setContent { BenchMateApp() }
+        setContent { BmContainer() }
     }
 }
 

9be1f64a0999257e1853cca2a8ee11a16a8d3114
Luke Simpson <lukesimpson@thegoodguys.com.au>
2024-02-04 17:49:02 +1100

    Add UI for empty state

--- app/src/main/java/app/benchmate/ui/features/bench/BenchScreen.kt
+++ app/src/main/java/app/benchmate/ui/features/bench/BenchScreen.kt
@@ -1,20 +1,31 @@
 package app.benchmate.ui.features.bench
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import app.benchmate.ui.components.BenchMateBottomAppBar
@@ -40,21 +51,46 @@ fun BenchScreen(
             }
         }
     ) {
-        
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .statusBarsPadding()
-                .navigationBarsPadding()
-                .padding(all = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
 
-        ) {
-            items(team.size) {
-                BmPlayerItem(firstName = team[it].firstName)
+        when (team.isEmpty()) {
+            true -> {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding()
+                        .navigationBarsPadding()
+                        .padding(all = 32.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ) {
+                    Image(
+                        modifier = Modifier.padding(16.dp),
+                        painter = rememberVectorPainter(image = Icons.Filled.Face),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit
+                    )
+                    Text(text = "Tap + below to create a player. You can then add them to the bench.")
+                }
+            }
+
+            false -> {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .statusBarsPadding()
+                        .navigationBarsPadding()
+                        .padding(all = 16.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+
+                    ) {
+                    items(team.size) {
+                        BmPlayerItem(firstName = team[it].firstName)
+                    }
+                }
             }
         }
-        
+
         if (openAlertDialog.value) {
             BmInputDialog(
                 onDismissRequest = { openAlertDialog.value = false },

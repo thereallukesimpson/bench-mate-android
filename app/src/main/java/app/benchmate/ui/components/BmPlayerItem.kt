50e76ddc880fb95a9948bbe6698fb7b9d0f30bd7
Luke Simpson <lukesimpson@thegoodguys.com.au>
2024-02-13 21:35:40 +1100

    Change card background to white

--- app/src/main/java/app/benchmate/ui/components/BmPlayerItem.kt
+++ app/src/main/java/app/benchmate/ui/components/BmPlayerItem.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -28,6 +30,9 @@ fun BmPlayerItem(
     Card(
         modifier = modifier
             .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        ),
         border = BorderStroke(width = 2.dp, color = player.status.getColour())
     ) {
         Row(

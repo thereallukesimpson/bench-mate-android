f4485c2104322b29139e66c309a9e0cfaccb685d
Luke Simpson <lukesimpson@thegoodguys.com.au>
2026-02-22 14:24:31 +1100

    Bump app version to 0.2.0
    This commit updates the app version for the 0.2.0 release.
    - `versionCode` is incremented from 5 to 6.
    - `versionName` is updated from "0.1.4" to "0.2.0".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.benchmate"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
